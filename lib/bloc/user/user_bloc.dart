d1e09a20a9b7d179e0ff5ae2e722e18b31629aee
hai061898 <truonghaiauto98@gmail.com>
2022-03-08 22:12:08 +0700

    post bloc

--- lib/bloc/user/user_bloc.dart
+++ lib/bloc/user/user_bloc.dart
@@ -1,7 +1,5 @@
 // ignore_for_file: non_constant_identifier_names, unused_element
 
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:social/models/response_user.dart';
