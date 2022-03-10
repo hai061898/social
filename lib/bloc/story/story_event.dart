79356477d132dfcdac4cf6e852a733e777273a92
hai061898 <truonghaiauto98@gmail.com>
2022-03-10 21:37:46 +0700

    story bloc

---
+++ lib/bloc/story/story_event.dart
@@ -0,0 +1,16 @@
+part of 'story_bloc.dart';
+
+@immutable
+abstract class StoryEvent {}
+
+class OnSelectedImagePreviewEvent extends StoryEvent {
+  final File image;
+
+  OnSelectedImagePreviewEvent(this.image);
+}
+
+class OnAddNewStoryEvent extends StoryEvent {
+  final String image;
+
+  OnAddNewStoryEvent(this.image);
+}
