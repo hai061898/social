f7c8d7b8eedac287071fd85f16d78d0095f524ea
hai061898 <truonghaiauto98@gmail.com>
2022-03-24 21:27:22 +0700

    profile

---
+++ lib/ui/screens/profile/theme_profile.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:social/ui/themes/colors.dart';
+import 'package:social/ui/widgets/widgets.dart';
+
+class ThemeProfilePage extends StatelessWidget {
+  const ThemeProfilePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title:
+            const TextCustom(text: 'Change Theme', fontWeight: FontWeight.w500),
+        elevation: 0,
+        leading: IconButton(
+          icon:
+              const Icon(Icons.arrow_back_ios_new_rounded, color: Colors.black),
+          onPressed: () => Navigator.pop(context),
+        ),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 10.0),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  TextCustom(text: 'Day'),
+                  Icon(Icons.radio_button_checked, color: ColorsSM.primary)
+                ],
+              ),
+              const SizedBox(height: 20.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  TextCustom(text: 'Evening'),
+                  Icon(Icons.radio_button_off_rounded)
+                ],
+              ),
+              const SizedBox(height: 20.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  TextCustom(text: 'System'),
+                  Icon(Icons.radio_button_off_rounded)
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
